3f3c1748e7c8eca9a8a9ac0427945d1769b44d6b
Sichang Li <sichang_li@apple.com>
2018-06-30 15:56:11 -0700

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import {PromotionService} from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
