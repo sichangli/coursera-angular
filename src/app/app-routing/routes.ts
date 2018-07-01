81df20ed08a3100eb0f470e41f1362330f9b0332
Sichang Li <sichang_li@apple.com>
2018-06-30 17:52:22 -0700

    Finished hw2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,7 +9,8 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'contactus',     component: ContactComponent },
   { path: 'dishdetail/:id',     component: DishdetailComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'aboutus', component: AboutComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {PromotionService} from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,7 @@ import {PromotionService} from './services/promotion.service';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
