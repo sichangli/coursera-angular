5ce8b8dc72a348ef595c61ae650855eb00e9f742
Sichang Li <sichang_li@apple.com>
2018-08-02 22:35:44 -0700

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,25 @@ import { Location } from '@angular/common';
 import {switchMap} from 'rxjs/operators';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Comment} from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
@@ -42,15 +56,24 @@ export class DishdetailComponent implements OnInit {
 
   @ViewChild('cform') commentFormDirective;
 
+  visibility = 'shown';
+
   constructor(private dishservice: DishService, private route: ActivatedRoute, private location: Location, private fb: FormBuilder,
               @Inject('BaseURL') public BaseURL) { }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(+params['id'])))
-      .subscribe(
-        dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); }, errmess => {
-          this.dish = null; this.errMess = <any>errmess; });
+    this.route.params.pipe(switchMap((params: Params) => {
+      this.visibility = 'hidden';
+      return this.dishservice.getDish(+params['id']);
+    }))
+      .subscribe(dish => {
+          this.dish = dish;
+          this.dishcopy = dish;
+          this.setPrevNext(dish.id);
+          this.visibility = 'shown';
+        },
+        errmess => this.errMess = <any>errmess);
     this.createForm();
   }
 
